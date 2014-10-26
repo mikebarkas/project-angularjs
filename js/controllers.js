6ead0b4e0f445a9419a162d9d87a6fa80ee01b86
Mike Barkas <mike@barkas.com>
2014-10-25 21:50:59 -0400

    Direction filter binding.

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,6 @@ var myNameSpace = angular.module('MyApp', []);
 myNameSpace.controller('MyController', ['$scope', '$http', function($scope, $http) {
   $http.get('js/data.json').success(function(data) {
     $scope.artists = data;
+    $scope.artistOrder = 'name';
   });
 }]);
